ba6d37a4843e5ca6a7a8d7b4fd58790164363f17
Wayne Liu <zxyzliu@gmail.com>
2019-06-28 14:42:02 -0500

    merge master

--- everyday_wechat/main.py
+++ everyday_wechat/main.py
@@ -6,7 +6,8 @@
 """
 import os
 import time
-from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 import itchat
 import random
 from itchat.content import *
@@ -33,10 +34,8 @@ def run():
     # 判断是否登录，如果没有登录则自动登录，返回 False 表示登录失败
     if not is_online(auto_login=True):
         return
-    set_system_notice('登录成功')
-    if conf.get('is_auto_relay'):
-        print('已开启图灵自动回复...')
-    init_alarm()  # 初始化定时任务
+
+
 
 
 def is_online(auto_login=False):
@@ -71,10 +70,10 @@ def is_online(auto_login=False):
             # 命令行显示登录二维码。
             itchat.auto_login(enableCmdQR=2, hotReload=hotReload, loginCallback=loginCallback,
                               exitCallback=exitCallback)
-            itchat.run(blockThread=False)
+            itchat.run(blockThread=True)
         else:
             itchat.auto_login(hotReload=hotReload, loginCallback=loginCallback, exitCallback=exitCallback)
-            itchat.run(blockThread=False)
+            itchat.run(blockThread=True)
         if _online():
             print('登录成功')
             return True
@@ -85,6 +84,8 @@ def is_online(auto_login=False):
 
 def init_wechat():
     """ 初始化微信所需数据 """
+    set_system_notice('登录成功')
+
     conf = get_yaml()
     itchat.get_friends(update=True)  # 更新好友数据。
     itchat.get_chatrooms(update=True)  # 更新群聊数据。
@@ -100,6 +101,11 @@ def init_wechat():
             print('自动回复中的好友昵称『{}』有误。'.format(name))
     # print(reply_userNames)
 
+
+    if conf.get('is_auto_relay'):
+        print('已开启图灵自动回复...')
+
+    init_alarm()  # 初始化定时任务
 
 def init_alarm():
     """ 初始化定时提醒 """
@@ -126,12 +132,12 @@ def init_alarm():
                   '(注意：必须要把需要的群聊保存到通讯录)'.format(group_name))
 
     # 定时任务
-    scheduler = BlockingScheduler()
+    scheduler = BackgroundScheduler()
     # 每天9：30左右给女朋友发送每日一句
     scheduler.add_job(send_alarm_msg, 'cron', hour=hour,
                       minute=minute, misfire_grace_time=15 * 60)
 
-    # 每隔 30 秒发送一条数据用于测试。
+    # # 每隔 30 秒发送一条数据用于测试。
     # scheduler.add_job(send_alarm_msg, 'interval', seconds=30)
 
     print('已开启定时发送提醒功能...')
@@ -142,6 +148,8 @@ def init_alarm():
 def text_reply(msg):
     """ 监听用户消息，用于自动回复 """
     try:
+        if not get_yaml().get('is_auto_relay'):
+            return
         # print(json.dumps(msg, ensure_ascii=False))
         # print(reply_userNames)
         # 获取发送者的用户id
